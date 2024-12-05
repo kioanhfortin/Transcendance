a914a6908adb010ecd18c39bb68a8d32eb1fff4f
oli <oivier_nault@hotmail.com>
2024-12-05 13:04:03 -0500

    add of a speed button and acceleration

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -1,16 +1,6 @@
 import * as THREE from 'three';
 
-export function notLooking(camera, object, threshold = 0.5) {
-    const cameraDirection = new THREE.Vector3();
-    camera.getWorldDirection(cameraDirection);
-
-    const objectPosition = new THREE.Vector3();
-    object.getWorldPosition(objectPosition);
-    const toObject = objectPosition.clone().sub(camera.position).normalize();
-    const dot = cameraDirection.dot(toObject);
-    return dot < threshold; // Return true if not looking
-}
-
+// quand la game se termine tout cacher
 export function hideGame(walls, players, ball) {
 	function setInvisible(obj) {
 		obj.visible = false;
@@ -20,43 +10,96 @@ export function hideGame(walls, players, ball) {
 	walls.forEach(setInvisible);
 }
 
-export function showGame(walls, players, ball, camera, realPoints) {
+// sert a montrer les elements du jeux
+export function showGame(walls, players, ball, camera, realPoints, isFourPlayer) {
 	function setVisible(obj) {
 		obj.visible = true;
         obj.quaternion.copy(camera.quaternion);
 	}
 	setVisible(ball);
+
 	players.forEach(setVisible);
-	walls.forEach(setVisible);
+	players[2].rotateZ(Math.PI / 2);
+	players[3].rotateZ(Math.PI / 2);
 
-    // qui commence avec la ball???
+	if (!isFourPlayer)
+	{
+		setVisible(walls[0]);
+		setVisible(walls[1]);
+		for (let i = 2; i != 6; i++) {
+			walls[i].visible = false;
+		}
+		players[3].visible = false;
+		players[2].visible = false;
+	}
+	else {
+		walls.forEach(setVisible);
+		setWall2v2PosRot(walls, camera);
+		for (let i = 2; i != 4; i++) {
+			players[i].position.x = 0;
+			players[i].position.z = 0;
+		}
+	}
     setBallPos(ball, 0);
-
-	let offset = 1.8;
+	// la distance pour les jouers 3 et 4
     players[0].position.x = (camera.position.z * -1);
 	players[0].position.z = (camera.position.x * 1);
 	players[1].position.x = (camera.position.z * 1);
 	players[1].position.z = (camera.position.x * -1);
 
+	setPosPoints(realPoints, isFourPlayer, camera);
 
-	
-	realPoints.forEach((point) => {
-		point.playerOne.position.x = (camera.position.z * -1) / offset;
-		point.playerOne.position.z = (camera.position.x * 1) / offset;
-		point.playerTwo.position.x = (camera.position.z * 1) / offset;
-		point.playerTwo.position.z = (camera.position.x * -1) / offset;
-		
-		point.playerOne.lookAt(camera.position);
-		point.playerOne.up.set(0, 1, 0);
-		point.playerTwo.lookAt(camera.position);
-		point.playerTwo.up.set(0, 1, 0);
-	})
-	realPoints[0].playerOne.visible = true;
-	realPoints[0].playerTwo.visible = true;
-
+	for (let i in realPoints[0]) {
+		realPoints[0][i].visible = true;
+	}
 }
 
 export function setBallPos(ball, wichPlayer) {
     ball.position.set(0, 0, 0);
     wichPlayer == 1 ? ball.translateX(-10) : wichPlayer == 2 ? ball.translateX(10) : false;
+}
+
+//set the position of all the points in 3D
+function setPosPoints(realPoints, isFourPlayer, camera) {
+	const offset = 1.8;
+	realPoints.forEach((point) => {
+		point.playerOne.position.x = (camera.position.z * -1) / offset;
+		point.playerOne.position.z = (camera.position.x * 1) / offset;
+		point.playerTwo.position.x = (camera.position.z * 1) / offset;
+		point.playerTwo.position.z = (camera.position.x * -1) / offset;
+		if (isFourPlayer)
+		{
+			const yOffset = 8;
+			point.playerOne.position.y = -yOffset;
+			point.playerTwo.position.y = yOffset;
+		}
+		for (let i in point) {
+			point[i].lookAt(camera.position);
+			point[i].up.set(0, 1, 0);
+        }
+	})
+}
+
+// set the postition and rotation of the extra walls when there is 4 players
+function setWall2v2PosRot(walls, camera) {
+	const yOffset = 20;
+	const offset = 0.95;	
+
+	const wallConfigs = [
+        { xFactor: -1, zFactor: 1, y: yOffset },
+        { xFactor: 1, zFactor: -1, y: -yOffset },
+        { xFactor: -1, zFactor: 1, y: -yOffset },
+        { xFactor: 1, zFactor: -1, y: yOffset },
+    ];
+
+	wallConfigs.forEach((config, index) => {
+        const wall = walls[index + 2];
+        wall.position.x = (camera.position.z * config.xFactor) / offset;
+        wall.position.z = (camera.position.x * config.zFactor) / offset;
+        wall.position.y = config.y;
+        wall.rotateX(Math.PI / 2);
+    });
+
+	walls[0].visible = false;
+	walls[1].visible = false;
 }
