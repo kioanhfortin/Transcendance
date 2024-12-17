75a8bd8284f25469edfc30124cd550717d7e7fe9
jeremy <jeremy.coquet38@gmail.com>
2024-12-17 11:41:41 -0500

    add backend with docker ok

--- backend/transcendance/urls.py
+++ backend/transcendance/urls.py
@@ -1,7 +1,8 @@
-"""transcendance URL Configuration
+"""
+URL configuration for transcendance project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,11 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from . import views
+from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-    path('', include('game.urls')),
 ]
