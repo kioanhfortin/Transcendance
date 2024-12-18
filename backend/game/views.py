12936891dab5eb049f14a49a1917d70912beb12f
jeremy <jeremy.coquet38@gmail.com>
2024-12-18 09:20:12 -0500

    install django CRF + change django version and create first APItest

--- backend/game/views.py
+++ backend/game/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.renderers import JSONRenderer
+
 # Create your views here.
+class HelloWorld(APIView):
+    renderer_classes = [JSONRenderer]
+    def get(self, request):
+        return Response({"message": "Hello, world!"}, status=status.HTTP_200_OK)
