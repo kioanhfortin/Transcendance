a0572b7cbda29434ae5c9b613a731c8e996dc3fc
Your Name <you@example.com>
2024-12-21 00:36:07 -0500

    essaie interpolation

--- frontend/js/PlayerCtrl.js
+++ frontend/js/PlayerCtrl.js
@@ -1,46 +1,43 @@
 import * as THREE from 'three';
 
-const yLimit = 13
-let reactionTime = 0;
+
+const 	yLimit = 13;
+// let		reactionTime = 0;
+const 	difficultyAi = 20;
 
 export function predictionBall(ball, speed) {
 	if (!ball || !ball.velocity) {
+		ball.velocity = new THREE.Vector3(0.2, 0.2, 0);
 		return ball.position.y;
 	}
-	const predictY = ball.position.y + ball.velocity.y * 20;
+	const predictY = ball.position.y + ball.velocity.y * difficultyAi;
 	return predictY;
 }
 
 export function playerControl(players, keys, game, ball) {
 	const speed = 0.35;
-	const smooth = 0.1;
-	const predictBall = predictionBall(ball, speed);
+	const smooth = 0.3;
+
+	const predictBall = predictionBall(ball, difficultyAi);
 	if (keys['w'] && players[0].position.y < yLimit)
 		players[0].position.y += speed;
 	else if (keys['s'] && players[0].position.y > -yLimit)
 		players[0].position.y -= speed;
 	if (game.isSinglePlayer) {
-		if (ball.position.y > players[1].position.y + 1) {
-			players[1].position.y += speed;
-		} else if (ball.position.y < players[1].position.y - 1) {
-			players[1].position.y -= speed;
+		// if (reactionTime >= 1) {
+			players[1].position.y = THREE.MathUtils.lerp(players[1].position.y, predictBall, smooth);
+			// reactionTime = 0;
 		}
-		// else
-		// {
-		// 	if (ball.position.y > players[1].position.y + 1) {
-		// 		players[1].position.y += speed;
-		// 	} else if (ball.position.y < players[1].position.y - 1) {
-		// 		players[1].position.y -= speed;
-		// 	}
-		// }
-	}
+		// reactionTime++;
+	// }
 	else {
 		if (keys['ArrowUp'] && players[1].position.y < yLimit)
-			players[1].position.y += speed;
+			players[1].position.y += 0.35;
 		else if (keys['ArrowDown'] && players[1].position.y > -yLimit)
 			players[1].position.y -= speed;
 		PlayerOther(players, keys, camera);
 	}
+
 	players.forEach(player => {
 		if (player.position.y > yLimit) player.position.y = yLimit;
 		if (player.position.y < -yLimit) player.position.y = -yLimit;
