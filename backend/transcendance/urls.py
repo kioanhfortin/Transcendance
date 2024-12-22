a78ab7b87f2f3ed3848582404f56db0d8c693ef9
jeremy <jeremy.coquet38@gmail.com>
2024-12-22 08:51:04 -0500

    add django jwt token and config + config back with rest_framework

--- backend/transcendance/urls.py
+++ backend/transcendance/urls.py
@@ -29,10 +29,24 @@ urlpatterns = [
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from game.views import HelloWorld  # Importez votre vue ici
+
+# Définir le routeur
+router = DefaultRouter()
+# Ajoutez vos vues API au routeur ici, par exemple :
+# router.register(r'games', GameViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('game.api')),
     path('', HelloWorld.as_view(), name='home'),  # Route pour la racine
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include(router.urls))
 ]
