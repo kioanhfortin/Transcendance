4bbb9970050873e3a307264cd874a8c5adbf7715
jeremy <jeremy.coquet38@gmail.com>
2025-01-01 21:04:51 -0500

    display stats in console OnClick statsButton OK

--- backend/game/views.py
+++ backend/game/views.py
@@ -19,14 +19,6 @@ class HelloWorld(APIView):
     def get(self, request):
         return Response({"message": "Hello, world!"}, status=status.HTTP_200_OK)
 
-# class UserRegistrationView(APIView):
-#     def post(self, request):
-#         serializer = UserRegistrationSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -41,7 +33,7 @@ class UserStatisticsView(APIView):
             serializer = UserStatisticsSerializer(statistics)
             return Response(serializer.data)
         except UserStatistics.DoesNotExist:
-            return Response({"error": "Statistics not found"}, status=404)
+            return Response({"error": "Statistics not found"}, status=400) # creer un jeu donnee vide?
 
 def log_request_data(get_response):
     def middleware(request):
