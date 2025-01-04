8a457037986402a62ee64f9e53c8f6f1d8046115
Your Name <you@example.com>
2025-01-04 18:55:46 -0500

    front for login, profil, stats, history

--- frontend/js/ui.js
+++ frontend/js/ui.js
@@ -259,3 +259,35 @@ document.addEventListener("DOMContentLoaded", function () {
     validateRegisterButton.addEventListener('click', showMainContent);
     logoutButton.addEventListener('click', handleLogout);
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const resetPasswordLink = document.getElementById('reset-password-link');
+    resetPasswordLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        alert('Redirecting to password reset page...');
+        // You can replace the alert with an actual redirect:
+        // window.location.href = '/reset-password';
+    })
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const uploadAvatarInput = document.getElementById('uploadAvatar');
+    const profilePicture = document.querySelector('.avatar-img');
+    const profileButton = document.getElementById("profileButton");
+
+    uploadAvatarInput.addEventListener('change', function() {
+        const file = uploadAvatarInput.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                profilePicture.src = e.target.result;
+                profileButton.innerHTML = `<img src="${e.target.result}" alt="Avatar" style="width: 30px; height: 30px; object-fit: cover; border-radius: 50%;">`;
+                profileButton.classList.remove("btn-primary");
+                profileButton.style.background = "transparent";
+                profileButton.style.border = "none";
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+});
