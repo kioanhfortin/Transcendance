91b09dd9e7d3db68db5052c15f4f92cd2a28b70d
jcoquet <jeremy.coquet38@gmail.com>
2025-01-14 10:17:06 -0500

    add cal API getHistory

---
+++ frontend/api/history.js
@@ -0,0 +1,68 @@
+import { getCookie } from "./cookie";
+
+export async function addHistory() {
+	const jwtToken = getCookie('access_token');
+
+	try {
+		const response = await fetch('http://localhost:8000/api/user-history/', {
+
+			method: 'POST',
+
+			headers: {
+
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${jwtToken}`,
+			},
+			body: JSON.stringify({
+				username: username,
+			}),
+		});
+
+		if (response.ok) {
+			const data = await response.json();
+			console.log("Add game in history", data);
+		} else {
+			const errorData = await response.json();
+			console.error('Error:', errorData);
+			alert('Failed to post history ' + errorData.detail);
+		}
+	} catch (error) {
+		console.error('Network error:', error);
+		alert('An error occurred while updating your data. Please try again later.');
+	}
+}
+
+export async function getHistory() {
+	const jwtToken = getCookie('access_token');
+	console.log("i'm here");
+	try {
+		const response = await fetch('http://localhost:8000/api/user-history/', {
+
+			method: 'GET',
+
+			headers: {
+
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${jwtToken}`,
+			},
+		});
+
+		if (response.ok) {
+			const data = await response.json();
+			console.log("list History: ", data);
+		} else {
+			const errorData = await response.json();
+			console.error('Error:', errorData);
+			alert('Failed to get history :  ' + errorData.detail);
+		}
+	} catch (error) {
+		console.error('Network error:', error);
+		alert('An error occurred while updating your list. Please try again later.');
+	}
+}
+
+export function handleHistory() {
+	document.getElementById('historyBtn').addEventListener('click', function() {
+		getHistory();
+	});
+}
